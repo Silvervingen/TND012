59e0aeb2b1ee719aeb56ed5ee6fe4cf3d8717aa3
Silvervingen <71696890+Silvervingen@users.noreply.github.com>
2023-12-09 22:06:08 +0100

    Add files via upload

---
+++ TND012 Duggor/Dugga 1 (v2)/Dugga1_HT2/Source.cpp
@@ -0,0 +1,85 @@
+// ============================================================
+// TND012: Dugga 1
+// Name: Antonio
+// Personal number: 9902268334
+// LiU-id: Antta839
+// ============================================================
+
+#include <iostream>
+/*
+
+using namespace std;
+
+int main() {
+
+	int choice = 0.0;
+	int values = 0.0;
+	int values2 = 0.0;
+	int values3 = 0.0;
+	int sum = 180;
+	int angles = 1.0;
+	int sides = 2.0;
+
+
+	cout << "Triangle validation (1-> angles, 2 -> sides): ";
+	cin >> choice;
+
+	if (sides != choice && choice != angles) // Test input option
+	{
+		cout << endl << "invalid option" << endl;
+	}
+
+
+	if (choice == angles) // Angels conditions
+	{
+		cout << "Enter angels: ";
+		cin >> values >> values2 >> values3;
+
+		// If an values is equal to zero or dont follow geometric requirements it writes invalid
+		if (values <= 0 || values2 <= 0 || values3 <= 0 || values + values2 + values3 != sum) {
+			cout << endl << "invalid" << endl;
+		}
+
+		else { // All three values must be equal to 180
+			if (values == values2 || values == values3 || values2 == values3) {
+				if (values == values2 && values2 == values3) { // If all three values is the same
+					cout << endl << "equilateral" << endl;
+				}
+				else { // if only two values is the same
+					cout << endl << "isosceles" << endl;
+				}
+			}
+			else {
+				cout << endl << "valid" << endl;
+			}
+		}
+	}
+
+	if (choice == sides)// Sides conditions
+	{
+		cout << "Enter sides: " << endl;
+		cin >> values >> values2 >> values3;
+
+		// If an values is equal to zero or dont follow geometric requirements it writes invalid
+		if (values <= 0 || values2 <= 0 || values3 <= 0 || values + values2 < values3 || values + values3 < values2 || values2 + values3 < values) {
+			cout << endl << "invalid" << endl;
+		}
+
+		else {
+			if (values == values2 || values == values3 || values2 == values3) {
+				if (values == values2 && values2 == values3) { // If all three values is the same
+					cout << endl << "equilateral" << endl;
+				}
+				else { // if only two values is the same
+					cout << endl << "isosceles" << endl;
+				}
+			}
+			else {
+				cout << endl << "valid" << endl;
+			}
+		}
+
+	}
+
+}
+*/
